9d5195204a34e20b0afec436cac9b83ecb84c1de
Rasukarusan <7gta0you1tube000@gmail.com>
2025-11-26 09:07:36 +0900

    eslint, prettier, jest導入

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    ignores: ["node_modules/**", "dist/**"],
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      globals: {
+        module: "readonly",
+        require: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+      },
+    },
+  }
+);
